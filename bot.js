aa286efd9075923fa542ae16db74e371d9aefc6e
ASLLALD <45972266+ASLLALD@users.noreply.github.com>
2019-02-26 18:11:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("546938764300255252")
+let channel =     client.channels.get("549963480535531532")
 setInterval(function() {
 channel.send(`, Dream , Dream , Dream , Dream , Dream , Dream ,
 Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , 
